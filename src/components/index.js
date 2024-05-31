f0a1a28ab85760b7d65faa74b242bbfbb4a2fe23
Raghav <90691016+RaghavKansal7@users.noreply.github.com>
2024-05-31 13:39:34 +0530

    .

---
+++ src/components/index.js
@@ -0,0 +1,6 @@
+export{ default as NavBar} from './Navbar';
+export{ default as Feed} from './Feed';
+export{ default as ChannelDetail} from './ChannelDetail';
+export{ default as SearchFeed} from './SearchFeed';
+export{ default as VideoDetail} from './VideoDetail';
+
